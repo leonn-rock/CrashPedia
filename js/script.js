d1b1a76c14c55ab7e8f312ac76819ced39340d08
leonn-rock <139873102+leonn-rock@users.noreply.github.com>
2023-08-31 12:01:36 -0300

    Update (Relacionados e Popup)
    Adição do Script do popup e começando a fazer o corpo da página bônus.

--- js/script.js
+++ js/script.js
@@ -43,3 +43,26 @@ btnRelacionados.on('click', irParaRelacionados)
 function irParaRelacionados(){
     $(location).attr('href', 'relacionados.html')
 }
+
+// ESTA PARTE DO SCRIPT É PARA O POPUP DA PÁGINA RELACIONADOS
+
+document.body.onload = mostraPopup
+
+const popup = $("#popup")
+const btnFecha = $("#fechar")
+
+btnFecha.on('click', fechaPopup)
+
+function mostraPopup(){
+    popup.toggle()
+    setTimeout(()=>{
+        popup.toggle()
+        popup.animate({left : "0"},1000)
+    },1000);
+}
+function fechaPopup(){
+    popup.animate({left : "-400px"},1000)
+    setTimeout(()=>{
+        popup.toggle()
+    },1000)
+}
